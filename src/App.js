2a303ab8c084dc961f576654151d7aee419aafc7
Shashi Kumar P <shashiessp@gmail.com>
2018-11-02 17:39:37 -0500

    init and add ip validator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
+
 class App extends Component {
+
+ constructor(props) {
+    super(props);
+
+    this.state = {
+      query: '',
+      ip: {},
+      error: null
+    }
+
+  }
+
+
+  componentDidMount() {
+    fetch("https://ip.nf/me.json")
+    .then(response => response.json())
+    .then(json => {
+      const ip = json.ip;
+      this.setState({ip});
+      this.setState({query:ip.ip});
+      console.log(this.state);
+    })
+  }
+  
+
+  search(){
+    const BASE_URL = 'https://ip.nf/'
+
+    const validate = require('ip-validator');
+
+    if(validate.ipv4(this.state.query))
+    {
+
+    const FETCH_URL = BASE_URL +  this.state.query +'.json'
+
+
+    fetch(FETCH_URL, {
+      method: 'GET'
+    })
+
+    .then(response => response.json())
+    .then(json => {
+      const ip = json.ip;
+      this.setState({ip});
+      this.setState({query:ip.ip});
+
+    });
+
+    }
+
+    this.setState({error: 'Invalid IP'});
+
+  }
+
+
+
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+             <div style={{marginTop: 50}}>
+                <h3>GET IP info</h3>
+                <form>
+                    <div className="form-group">
+                        <label>IP Address:  </label>
+                        {this.state.error}
+                        <input type="text" value={this.state.query}
+                          onChange={event => {this.setState({query: event.target.value})}}
+                         className="form-control"/>
+                    </div>
+
+                    <div className="form-group">
+                        <input type="button" onClick={() => this.search()} value="Get Info" className="btn btn-primary"/>
+                    </div>
+                </form>
+
+                <p> IP: {this.state.ip.ip} </p>
+                <p> ASN: {this.state.ip.asn} </p>
+                <p> City: {this.state.ip.city} </p>
+                <p> country code: {this.state.ip.country} </p>
+                <p> Lat: {this.state.ip.latitude} </p>
+                <p> long: {this.state.ip.longitude} </p>
+                <p> Postcode: {this.state.ip.post_code} </p>
+            </div>
+
       </div>
     );
   }
